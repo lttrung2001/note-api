aaf08f4ba27c3a378897625aa8458ef2d2ce7420
Le Thanh Trung <lt.trung2001@gmail.com>
2023-01-03 18:50:59 +0700

    Update deleteImageService.js

--- services/image/deleteImageService.js
+++ services/image/deleteImageService.js
@@ -4,7 +4,7 @@ const { app } = require('../../utils/firebase')
 const { getStorage, ref, deleteObject } = require('firebase/storage')
 
 const deleteImage = async (req, res) => {
-    const imageUrl = decodeURI(req.query.url)
+    const imageUrl = decodeURIComponent(req.query.url)
     const noteId = req.query.noteId
 
     if (imageUrl == null || noteId == null) {
