5cb1389f82c2ac19b404f2d4dfeb86701a9cfc17
Le Thanh Trung <lt.trung2001@gmail.com>
2023-01-03 17:52:13 +0700

    await and check requirement

--- services/image/deleteImage.js
+++ services/image/deleteImage.js
@@ -4,8 +4,17 @@ const { app } = require('../../utils/firebase')
 const { getStorage, ref, deleteObject } = require('firebase/storage')
 
 const deleteImage = async (req, res) => {
+    console.log(req.query)
     const imageUrl = req.query.url
     const noteId = req.query.noteId
+
+    if (imageUrl == null || noteId == null) {
+        return res.status(code.bad_request).json({
+            code: code.bad_request,
+            message: 'At least 1 argument is required',
+            data: null
+        })
+    }
 
     const storage = getStorage(app)
     const imageRef = ref(storage, imageUrl)
@@ -19,13 +28,13 @@ const deleteImage = async (req, res) => {
             images: imagesAfterRemove
         })
 
-        res.status(code.success).json({
+        return res.status(code.success).json({
             code: code.success,
             message: "Delete image successfully",
             data: imageUrl
         })
     } catch (error) {
-        res.status(code.internal_server_error).json({
+        return res.status(code.internal_server_error).json({
             code: code.internal_server_error,
             message: "Delete image failed",
             data: null
