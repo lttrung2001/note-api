f9947089ce47625f92ee7fd73df9455a7a838884
Le Thanh Trung <lt.trung2001@gmail.com>
2023-01-04 16:01:28 +0700

    Update editNote.js

--- services/note/editNote.js
+++ services/note/editNote.js
@@ -44,7 +44,7 @@ const editNote = async (req, res) => {
                             )
                         }
                         // Wait until all images uploaded
-                        noteEdit.images.push(await Promise.all(promiseArray))
+                        noteEdit.images = await Promise.all(promiseArray)
                     }
                 }
                 // Save note
